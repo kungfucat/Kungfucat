00a27d8598d123cef50d45c6be2e5788ea7659b7
harsh011 <harshbhardwaj2357@gmail.com>
2017-10-03 11:15:16 +0530

    cancel message added

--- app/src/main/java/com/example/harsh/kungfucat/MainActivity.java
+++ app/src/main/java/com/example/harsh/kungfucat/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancel() {
-
+                Log.i("Cancelled", "Cancel");
             }
 
             @Override
